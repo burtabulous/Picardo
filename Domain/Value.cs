e12c03339f0619560dbb81800ea0f1a1fd644f21
Burt Fous <Burt.Fous@FiveSixTwo.com>
2020-07-19 12:20:32 -0700

    initial commit

---
+++ Domain/Value.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Domain
+{
+    public class Value
+    {
+        public int Id {get;set;} //entity framework automatically uses this as seed
+        public string Name {get;set;}
+    }
+}
